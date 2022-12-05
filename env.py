e6148d20fcc924b5d06980faa99f2427d37ea2dc
vellickson <84603089+vellickson@users.noreply.github.com>
2022-12-05 13:35:05 -0800

    Update env.py

--- env.py
+++ env.py
@@ -6,3 +6,4 @@ CFDB_USER = environ.get("CFDB_USER", 'postgres')
 CFDB_PASSWORD = environ.get("CFDB_PASSWORD", 'postgres')
 CFDB_NAME = environ.get("CFDB_NAME", 'cfb_rankings')
 CONNECTION_STRING = f"dbname={CFDB_NAME} user={CFDB_USER} password={CFDB_PASSWORD} host={CFDB_HOST} port={CFDB_PORT}"
+BEARER = environ.get("BEARER")
